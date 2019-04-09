a8fd41734bd9346fcabc93164c82f0aa633807d9
Richard Zhu <rzhu.96@gmail.com>
2019-04-09 11:19:08 -0700

    fix typo in names

--- shell-scripts/paste-urls.py
+++ shell-scripts/paste-urls.py
@@ -8,7 +8,7 @@ now = datetime.datetime.now()
 
 base_dir = os.path.expanduser('~/notes')
 
-md_basename = now.strftime('%Y-%m-%d-urls.md')
+md_basename = now.strftime('%Y_%m_%d_urls.md')
 md_path = os.path.join(base_dir, md_basename)
 
 with open(md_path, 'a+') as fp:
