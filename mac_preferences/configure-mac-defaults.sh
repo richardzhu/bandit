9a3958f2dbc303d3edecd007b8a1cab6d13dd0e8
Richard Zhu <rzhu.96@gmail.com>
2020-03-18 13:17:19 -0500

    .

---
+++ mac_preferences/configure-mac-defaults.sh
@@ -0,0 +1,3 @@
+# setup SelfControl defaults in minutes: max block length 5 days, intervals of 2 hours
+defaults write org.eyebeam.SelfControl MaxBlockLength -int 7200
+defaults write org.eyebeam.SelfControl BlockLengthInterval -int 120
--- shell-scripts/watch.sh
+++ shell-scripts/watch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/9574089/osx-bash-watch-command
+# http://daniel.lubarov.com/simple-watch-script-for-osx
 # usage: watch.sh <your_command> <sleep_duration>
 
-while :;
-  do
+while :; do
   clear
   date
-  $1
-  sleep ${2:3}  # if no duration provided, refresh every 3s
+  bash -c "$1"
+  sleep ${2:-5}  # if no duration provided, refresh every 5s
 done
