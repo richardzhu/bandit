13c7f0a5816d20ceeaab93844ff73bb4a20e03b8
Richard Zhu <rzhu.96@gmail.com>
2020-10-15 00:55:37 -0500

    set mac defaults

--- mac_preferences/configure-mac-defaults.sh
+++ mac_preferences/configure-mac-defaults.sh
@@ -1,3 +1,6 @@
 # setup SelfControl defaults in minutes: max block length 5 days, intervals of 2 hours
 defaults write org.eyebeam.SelfControl MaxBlockLength -int 7200
 defaults write org.eyebeam.SelfControl BlockLengthInterval -int 120
+
+# https://superuser.com/questions/1498967/how-to-always-open-certain-app-links-in-chrome-without-prompt/1545311
+defaults write com.brave.Browser ExternalProtocolDialogShowAlwaysOpenCheckbox -bool true
